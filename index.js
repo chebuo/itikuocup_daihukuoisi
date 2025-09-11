c9016679e85158da4f2f2bec06f0e8dc8779d09a
daihuku281 <keitaguojing3@gmail.com>
2025-09-11 16:54:05 +0900

    [add]#1絞り込みとチャックボタンの追加

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const todo = document.getElementById('todo');
 const priority = document.querySelector('select');
 const deadline = document.querySelector('input[type="date"]');
 const submit = document.getElementById('submit');
+let list = [];
 submit.addEventListener('click', () => {
     console.log('jaja');
     const item = {};
@@ -11,17 +12,57 @@ submit.addEventListener('click', () => {
     item.priority = priority.value;
     item.deadline = deadline.value;
     item.done = false;
-
+    if (deadline.value != ''){
+            item.deadline = deadline.value;
+        }else{
+            window.alert('期日を入力してください');
+        const date = new Date();
+        item.deadline = date.toLocaleDateString();}
+        if (todo.value != ''){
+            item.todo = todo.value;
+        }else{
+            item.todo = 'ダミーTODO';
+        }
+        
     console.log(item);
    todo.value ='';
    priority.value = '普';
    deadline.value = '';
    const tr = document.createElement('tr');
 
-        for (const prop in item) {
+        for (const prop in item){
             const td = document.createElement('td');
-            td.textContent = item[prop];
+            if (prop == 'done'){
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.checked = item[prop];
+                td.appendChild(checkbox);
+            }else{
+                td.textContent = item[prop];
+            }
             tr.appendChild(td);
         }
+        
         table.append(tr);
-});
+        });
+
+        const filterButton = document.createElement('button');
+        filterButton.textContent = '優先度（高）で絞り込み';
+        filterButton.id = 'priority';
+        const main = document.querySelector('main');
+        main.appendChild(filterButton);
+
+        filterButton.addEventListener('click', () => {
+            const trList = Array.from(document.getElementsByTagName('tr'));
+            trList.shift();
+            for (const tr of trList) {
+                tr.remove();
+            }
+       // table.textContent = '';
+
+            for (const item of list) {
+                if (item.priority == '高'){
+                    addItem(item);
+                }
+            }
+        });
